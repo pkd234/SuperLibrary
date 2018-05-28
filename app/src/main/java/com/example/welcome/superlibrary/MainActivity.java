498291b3cca246dd6d0ad942db082f58bf500e31
PANKAJ KUMAR <pkd.234@gmail.com>
2018-05-28 07:51:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/welcome/superlibrary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.welcome.superlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.superlibrarycode.Quick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ superlibrarycode/src/main/java/com/example/superlibrarycode/Quick.java
@@ -0,0 +1,10 @@
+package com.example.superlibrarycode;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Quick {
+    public static void ToastQuick(Context context,String dd){
+        Toast.makeText(context, dd, Toast.LENGTH_SHORT).show();
+    }
+}
